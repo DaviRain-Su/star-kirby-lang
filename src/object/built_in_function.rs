2ebdd162eb97d0c944ea53d4cf41480acfc01c0b
Davirain <davirain.yin@gmail.com>
2022-07-23 19:14:16 +0800

    add array fist last rest push function

--- src/object/built_in_function.rs
+++ src/object/built_in_function.rs
@@ -3,6 +3,9 @@ use crate::object::integer::Integer;
 use crate::object::{Object, ObjectInterface, ObjectType};
 use std::any::Any;
 use std::fmt::{Display, Formatter};
+use crate::NULL;
+use crate::object::array::Array;
+use crate::object::ObjectType::ARRAY_OBJ;
 
 #[derive(Debug, Clone)]
 pub struct Builtin {
@@ -10,9 +13,9 @@ pub struct Builtin {
 }
 
 impl Builtin {
-    pub fn new() -> Self {
+    pub fn new(func: fn(Vec<Object>) -> anyhow::Result<Object>) -> Self {
         Self {
-            built_in_function: Box::new(process_len),
+            built_in_function: Box::new(func),
         }
     }
 }
@@ -23,7 +26,7 @@ impl Display for Builtin {
     }
 }
 
-fn process_len(args: Vec<Object>) -> anyhow::Result<Object> {
+pub fn process_len(args: Vec<Object>) -> anyhow::Result<Object> {
     if args.len() != 1 {
         return Err(anyhow::anyhow!(format!(
             "wrong number of arguments. got={}, want=1",
@@ -32,15 +35,113 @@ fn process_len(args: Vec<Object>) -> anyhow::Result<Object> {
     }
 
     match args[0].clone() {
-        Object::String(string_obj) => Ok(Object::Integer(Integer {
+        Object::String(string_obj) => Ok(Integer {
             value: string_obj.value.len() as i64,
-        })),
+        }.into()),
+        Object::Array(array) => Ok(Integer {
+            value: array.elements.len() as i64,
+        }.into()),
         _ => Err(anyhow::anyhow!(format!(
             "argument to `len` not supported, got {}",
             args[0].r#type()
         ))),
     }
 }
+
+pub fn array_first_element(args: Vec<Object>) -> anyhow::Result<Object> {
+    if args.len() != 1 {
+        return Err(anyhow::anyhow!(format!(
+            "wrong number of arguments. got={}, want=1",
+            args.len()
+        )));
+    }
+
+
+    if args[0].r#type() != ARRAY_OBJ {
+        return Err(anyhow::anyhow!("argument to `first` must ARRAY, got {}", args[0].r#type()));
+    }
+
+    match args[0].clone() {
+        Object::Array(array) if array.elements.len() > 0 =>  {
+            Ok(*array.elements[0].clone().clone())
+        }
+        _ => Ok(NULL.into()),
+    }
+}
+
+pub fn array_last_element(args: Vec<Object>) -> anyhow::Result<Object> {
+    if args.len() != 1 {
+        return Err(anyhow::anyhow!(format!(
+            "wrong number of arguments. got={}, want=1",
+            args.len()
+        )));
+    }
+
+
+    if args[0].r#type() != ARRAY_OBJ {
+        return Err(anyhow::anyhow!("argument to `first` must ARRAY, got {}", args[0].r#type()));
+    }
+
+    match args[0].clone() {
+        Object::Array(array) if array.elements.len() > 0 =>  {
+            let length = array.elements.len();
+            Ok(*array.elements[length - 1].clone().clone())
+        }
+        _ => Ok(NULL.into()),
+    }
+}
+
+
+pub fn array_rest_element(args: Vec<Object>) -> anyhow::Result<Object> {
+    if args.len() != 1 {
+        return Err(anyhow::anyhow!(format!(
+            "wrong number of arguments. got={}, want=1",
+            args.len()
+        )));
+    }
+
+
+    if args[0].r#type() != ARRAY_OBJ {
+        return Err(anyhow::anyhow!("argument to `first` must ARRAY, got {}", args[0].r#type()));
+    }
+
+    match args[0].clone() {
+        Object::Array(array) if array.elements.len() > 0 =>  {
+            let mut new_elements = array.elements.clone();
+            new_elements.remove(0);
+            Ok(Array {
+                elements: new_elements,
+            }.into())
+        }
+        _ => Ok(NULL.into()),
+    }
+}
+
+pub fn array_push_element(args: Vec<Object>) -> anyhow::Result<Object> {
+    if args.len() != 2 {
+        return Err(anyhow::anyhow!(format!(
+            "wrong number of arguments. got={}, want=2",
+            args.len()
+        )));
+    }
+
+
+    if args[0].r#type() != ARRAY_OBJ {
+        return Err(anyhow::anyhow!("argument to `first` must ARRAY, got {}", args[0].r#type()));
+    }
+
+    match args[0].clone() {
+        Object::Array(array) =>  {
+            let mut array = array.elements.clone();
+            array.push(Box::new(args[1].clone()));
+            return Ok(Array {
+                elements: array,
+            }.into())
+        }
+        _ => Ok(NULL.into()),
+    }
+}
+
 
 impl ObjectInterface for Builtin {
     fn r#type(&self) -> ObjectType {
